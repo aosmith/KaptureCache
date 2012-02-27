e31cf76a7712a5ee13368288bd5b9cdd0bd88e46
Alex Smith <aosmith@gmail.com>
2012-02-27 15:16:13 -0600

    Initial Commit

---
+++ KaptureCacheTests/KaptureCacheTests.h
@@ -0,0 +1,13 @@
+//
+//  KaptureCacheTests.h
+//  KaptureCacheTests
+//
+//  Created by Alexander Smith on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KaptureCacheTests : SenTestCase
+
+@end
---
+++ KaptureCacheTests/KaptureCacheTests.m
@@ -0,0 +1,32 @@
+//
+//  KaptureCacheTests.m
+//  KaptureCacheTests
+//
+//  Created by Alexander Smith on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KaptureCacheTests.h"
+
+@implementation KaptureCacheTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KaptureCacheTests");
+}
+
+@end
